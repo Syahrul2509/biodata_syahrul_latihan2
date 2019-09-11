d967a933d8fe49ebfae3e6a691c42a66645b3528
Syahrul2509 <dumbledore2509@gmail.com>
2019-09-12 06:07:44 +0700

    tugas biodata

---
+++ app/src/main/java/com/syahrul/biodata_syahrul_latihan2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.syahrul.biodata_syahrul_latihan2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }public void onClick(View view){
+        Intent move = new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(move);
+    }
+}
